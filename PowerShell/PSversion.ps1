559e1afc652ccdc831dac8323993a19fde5ec669
NathanPBarnes <70610511+NathanPBarnes@users.noreply.github.com>
2022-03-14 21:35:47 -0700

    updating PowerShell

--- PowerShell/PSversion.ps1
+++ PowerShell/PSversion.ps1
@@ -1,9 +1,26 @@
+<# The script below can be used to determine various software versions for quick comparisons
+
+
+# https://docs.microsoft.com/en-us/dotnet/api/system.version.-ctor?view=netframework-4.0
+#>
+
+
+# need to utilize version & the objects methods for comparision of versions of software
 filter Convert-VersionToComparableText { '{0:0000000000}{1:0000000000}{2:0000000000}' -f $_.Major, $_.Minor, $_.Build }
-$SomeVersion = [Version]'7.1.1' | Convert-VersionToComparableText
+$SomeVersion = [Version]'9.4.2' | Convert-VersionToComparableText
 
 $scriptversion = $SomeVersion
-$currentVersion = $PSVersionTable.PSVersion
+$currentVersion = $PSVersionTable.PSVersion 
 
-if ([Version]::new($scriptversion.Major,$scriptversion.Minor,$scriptversion.Build) -lt [Version]::new($currentVersion.Major,$currentVersion.Minor,$currentVersion.Build)) {
-    Write-Output "succes"
+if ([Version]::new($scriptversion.Major,$scriptversion.Minor,$scriptversion.Build) -gt [Version]::new($currentVersion.Major,$currentVersion.Minor,$currentVersion.Build)) 
+
+{
+    Write-Output "success"
+}
+
+elseif ([Version]::new($scriptversion.Major,$scriptversion.Minor,$scriptversion.Build) -lt [Version]::new($currentVersion.Major,$currentVersion.Minor,$currentVersion.Build))
+
+{
+    Write-Output "fail"
+
 }
